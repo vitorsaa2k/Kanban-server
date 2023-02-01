5ecb9343c36db6bd4cf4c28316a3012a1e60d195
vitoreow <vitoradriel66@gmail.com>
2023-01-31 21:33:00 -0300

    algorithm of the drag and drop flow

--- Routes/User.js
+++ Routes/User.js
@@ -4,6 +4,107 @@ const User = require('../Models/User')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
 
+let user = [
+  {
+    title: 'To do',
+    color: '#0080FE',
+    tasks: [
+      {
+        task: 'Center the div',
+        isCritical: false,
+        isDone: false,
+        id: '3JSDDN'
+      },
+      {
+        task: 'newArray',
+        isCritical: false,
+        isDone: false,
+        id: 'PASIDA9'
+      },
+    ]
+  },
+  {
+    title: 'Doing',
+    color: '#F08538',
+    tasks: [
+      {
+        task: 'Create a new button',
+        isCritical: false,
+        isDone: false,
+        id: 'KLASJ3'
+      }
+    ]
+  },
+  {
+    title: 'Done',
+    color: '#79A41F',
+    tasks: [
+      {
+        task: 'Change the input color',
+        isCritical: false,
+        isDone: false,
+        id: 'ASDSA'
+      }
+    ]
+  },
+  {
+    title: 'a',
+    color: '',
+    tasks: [
+      {
+        task: 'test',
+        isCritical: false,
+        isDone: false,
+        id: 'FDG61'
+      }
+    ]
+  },
+  {
+    title: 'b',
+    color: '',
+    tasks: [
+      {
+        task: 'Change the input color',
+        isCritical: false,
+        isDone: false,
+        id: 'ASDFS7'
+      },
+    ]
+  },
+  {
+    title: 'c',
+    color: '',
+    tasks: [
+      {
+        task: 'Change the input color',
+        isCritical: false,
+        isDone: false,
+        id: 'ASFAS645'
+      }
+    ]
+  },
+]
+
+function updateToDos(data) {
+  let destination = {...user.filter((user) => user.title === data.destination.droppableId)}
+  let destinationIndex = user.indexOf(destination[0])
+  let source = {...user.filter((user) => user.title === data.source.droppableId)}
+  let sourceIndex = user.indexOf(source[0])
+
+  if(data.destination.droppableId === data.source.droppableId) {
+    let dest = {...source[0].tasks[data.source.index]}
+    let sour = {...destination[0].tasks[data.destination.index]}
+    user[destinationIndex].tasks[data.destination.index] = dest
+    user[sourceIndex].tasks[data.source.index] = sour
+
+  } else {
+    user[destinationIndex].tasks.splice(data.destination.index, 0, source[0].tasks[data.source.index])
+    user[sourceIndex].tasks.splice(data.source.index, 1)
+  }
+  
+  return user
+}
+
 
 Router.post('/signin', async (req, res) => {
 
@@ -16,8 +117,13 @@ Router.post('/signin', async (req, res) => {
 
 })
 
+Router.post('/kanban/update', async (req, res) => {
+  let newBoard = updateToDos(req.body)
+  res.json(newBoard)
+})
+
 Router.get('/kanban', authenticateToken, async (req, res) => {
-  const user = await User.find({name: req.user.name})
+  /* const user = await User.find({name: req.user.name}) */
   res.json(user)
   
 })
